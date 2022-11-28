a2bf098d91e02d037cbd0e8fd4eff2d9655d5014
Karmele Elorza <elorza.karmele@uni.eus>
2022-11-28 10:09:17 +0100

    Ardoak restapi- web

---
+++ 2.ebaluaketa/WineShop/Wineshop/Services/IArdoaService.cs
@@ -0,0 +1,10 @@
+﻿using Wineshop.Models;
+
+namespace Wineshop.Services
+{
+    public interface IArdoaService
+    {
+        Task<IList<ArdoaUpeltegi>> GetMota(int id);
+        //Task<IList<Ardoa>> GetArdoak(int id);
+    }
+}
---
+++ 2.ebaluaketa/WineShop_Zaharra/ViewComponents/ArdoakViewComponents.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WineShop.Services;
+
+namespace WineShop.ViewComponents
+{
+    public class ArdoakViewComponents : ViewComponent
+
+    {
+        private readonly IArdoaService _ardoaService;
+        public ArdoakViewComponents(IArdoaService ardoaService)
+        {
+            _ardoaService = ardoaService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            return View(await _ardoaService.GetMota(id));
+        }
+    }
+}
